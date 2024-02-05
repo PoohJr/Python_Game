09efe3afa2f594bbec66b1722d12af58e79cf312
PoohJr <Williamdomenech06@gmail.com>
2024-02-05 13:53:40 -0500

    study this
    i most ;iley finished but study the elemnets of how i got it

--- pig.py
+++ pig.py
@@ -8,10 +8,10 @@ def roll():
     return roll
 
 value = roll()
-print(value)
+
 
 while True:
-    players = input("Enter the number of players (2-4) ")
+    players = input("Enter the number of players (2 - 4): ")
     if players.isdigit():
         players = int(players)
         if 2 <= players <= 4:
@@ -25,22 +25,33 @@ player_scores = [0 for _ in range(players)]
 
 while max(player_scores) < max_score:
 
-    current_score = 0
+     for player_idx in range(players):
+        print("\nPlayer Number" , player_idx + 1, "Turn Just Started!")
+        print("Your total score is:", player_scores[player_idx], "\n")
+        current_score = 0
 
-    should_roll = input("Would You like to Roll (y)? ")
-    if should_roll.lower() == "y":
-        break
+        while True:
+            should_roll = input("Would You like to Roll (y)? ")
+            if should_roll.lower() != "y":
+                break
 
-    value = roll()
-    if value == 1:
-        print("ypu just rolled a 1! Turn Done!")
-        break
-    else:
-        current_score += value
-        print("You Rolled a " , value)
+            value = roll()
+            if value == 1:
+                print("you just rolled a 1! Turn Done!")
+                break
+            else:
+                current_score += value
+                print("You Rolled a " , value)
 
-    print("Your Score is", current_score)
+            print("Your Score is", current_score)
+            
+        player_scores[player_idx] += current_score
+        print("Your Total score is:", player_scores[player_idx])
 
+max_score = max(player_scores)
+player_winning = player_scores.index(max_score)
+print("Player Number", player_winning + 1, 
+      "is the winner with a score of:", max_score )
 
 
     
