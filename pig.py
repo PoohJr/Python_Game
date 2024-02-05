f04962f1b6352dff1b5d249da2f96be93e46259b
PoohJr <Williamdomenech06@gmail.com>
2024-02-05 10:46:34 -0500

    some

--- pig.py
+++ pig.py
@@ -9,3 +9,36 @@ def roll():
 
 value = roll()
 print(value)
+
+while True:
+    players = input("Enter the number of players (2-4) ")
+    if players.isdigit():
+        players = int(players)
+        if 2 <= players <= 4:
+            print("must be between 2-4 players: ")
+            break
+        else:
+            print("invalid, Try Again")
+
+max_score = 50
+player_scores = [0 for _ in range(players)]
+
+while max(player_scores) < max_score:
+
+    current_score = 0
+
+    should_roll = input("Would You like to Roll (y)? ")
+    if should_roll.lower() == "y":
+        break
+
+    value = roll()
+    if value == 1:
+        print("ypu just rolled a 1! Turn Done!")
+        break
+    else:
+        current_score += value
+        print("You Rolled a " , value)
+
+    print("Your Score is", current_score)
+
+    
